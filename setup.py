2650edb01bf1b30917242ad2477b4d7f35c77118
Troy H. Deck <tdeck@wustl.edu>
2015-02-09 19:15:16 -0800

    Set up pip package.

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='rodong',
+    version='1.0.0',
+    description='Scrape articles from Rodong Sinumn, the North Korean news agency',
+    long_description=read('README.md'),
+    url='https://github.com/tdeck/rodong',
+    license='MIT',
+    author='Troy Deck',
+    py_modules=['rodong'],
+    install_requires=[
+        'lxml==3.4.2',
+        'requests==2.5.1'
+    ],
+    include_package_data=True
+)
